49aa20d394411436cf415a148b13c54d39b38f02
AF Tamim <itamim345@gmail.com>
2023-03-10 20:53:34 +0530

    Structured the program in a function

--- findSmallestInArray.js
+++ findSmallestInArray.js
@@ -1,8 +1,11 @@
-let arr = [23,43,2,12,500,45,1,76,5,7,13];
-// sort the array
-arr.sort(function(a,b){
-    return a-b;
-})
-//console log the 1st index to get min value as We sorted the array
-console.log(arr[0]);
-
+let arr = [23,43,2,12,500,0,45,1,76,5,7,13];
+function findSmallest(){
+  // sort the array
+  arr.sort(function (a, b) {
+    return a - b;
+  });
+  //return 1st index to get min value as We sorted the array
+  return (arr[0]);
+}
+// Console log to print the value in console
+console.log(findSmallest());
