8c1879a9e6cb8f861025d1f63f7d6d3b25bc81c2
AF Tamim <itamim345@gmail.com>
2023-03-10 20:48:49 +0530

    smallestInArray

---
+++ findSmallestInArray.js
@@ -0,0 +1,8 @@
+let arr = [23,43,2,12,500,45,1,76,5,7,13];
+// sort the array
+arr.sort(function(a,b){
+    return a-b;
+})
+//console log the 1st index to get min value as We sorted the array
+console.log(arr[0]);
+
